8e33e5733bc7aa720980cbb9fff038ed269f8a96
R0DlON <rodionprokopchenkoo@gmail.com>
2021-10-14 17:44:37 +0300

    попытка вывода текста 1

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import './sass/main.scss';
+import countriesAll from './templates/countries.hbs';
 import Fetch from './partials/fetchCountries';
-import countries from './templates/countries.hbs';
 
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
   countriesContainer: document.querySelector('.countriesConrainer'),
 };
-console.log(refs.searchForm);
-console.log(refs.countriesContainer);
+
 const fetch = new Fetch();
-console.log(fetch);
 
 refs.searchForm.addEventListener('input', onSearch);
+console.log(fetch.fetchCountries(fetch));
 
-function onSearch(e) {
-  if (fetch.queary === '') {
-    return alert('Введи что-то нормальное');
-  }
-  fetch.queary = e.currentTarget.elements.query.value;
-
-  console.log(fetch);
-  appendCountries();
+function createCountries(countries) {
+  console.log(countriesAll(countries));
+  return countriesAll(countries);
 }
 
 function appendCountries(countries) {
-  refs.countriesContainer.insertAdjacentHTML('beforeend', countries);
+  refs.countriesContainer.insertAdjacentHTML('beforeend', createCountries(countries));
+}
+
+function onSearch(e) {
+  fetch.queary = e.currentTarget.elements.query.value;
+  fetch.fetchCountries();
+  appendCountries(countries);
 }
--- src/partials/fetchCountries.js
+++ src/partials/fetchCountries.js
@@ -1,24 +1,22 @@
-const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
 const BASE_URL = 'https://restcountries.com/v2';
-const options = {
-  headers: {
-    Authorization: API_KEY,
-  },
-};
 
 export default class Fetch {
   constructor(searchQueary) {
     this.searchQueary = '';
   }
 
-  fetchArticles() {
+  fetchCountries() {
     const url = `${BASE_URL}/name/${this.searchQueary}`;
-    fetch(url, options)
-      .then(r => r.json())
-      .then(({ countries }) => {
+    console.log(url);
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(countries => {
+        console.log(countries);
         return countries;
       });
   }
+
   set queary(newQuery) {
     this.searchQueary = newQuery;
   }
